b979d911fb78df92a04bc1ae5f8e5e3c6564f2c1
Alexander Daniel <alexander.daniel@gmx.at>
2014-01-21 13:01:18 +0100

    Bridging Flying Saucer logging to SLF4J and added a stop watch

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ and use `li:before`. You can view it your browser of choice or run the
 [App](src/main/java/lachdrache/App.java) to generate the PDF with
 [Flying Saucer](https://code.google.com/p/flying-saucer/).
 
+[App](src/main/java/lachdrache/App.java) also demonstrates how to
+bridge/redirect Flying Saucer logging to [SLF4J](http://www.slf4j.org).
+
+
--- src/main/java/lachdrache/App.java
+++ src/main/java/lachdrache/App.java
@@ -1,5 +1,8 @@
 package lachdrache;
 
+import org.apache.commons.lang3.time.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 import org.xhtmlrenderer.util.XRLog;
 
@@ -9,9 +12,19 @@ import java.util.logging.Level;
 
 public class App {
 
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) throws Exception {
         configureLogging();
+
+        logger.info("Starting PDF generation");
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
         generatePdf();
+
+        stopWatch.stop();
+        logger.info("PDF generation took " + stopWatch.getTime() + "ms");
     }
 
     private static void generatePdf() throws Exception {
@@ -21,10 +34,13 @@ public class App {
         renderer.createPDF(new FileOutputStream(new File("target/emptyListItems.pdf")), true);
     }
 
+    /**
+     * http://www.slf4j.org/api/org/slf4j/bridge/SLF4JBridgeHandler.html
+     */
     private static void configureLogging() {
         System.getProperties().setProperty("xr.util-logging.loggingEnabled", "true");
-        XRLog.setLoggingEnabled(true);
-        XRLog.setLevel("", Level.WARNING);
+        System.getProperties().setProperty("xr.util-logging.handlers", "org.slf4j.bridge.SLF4JBridgeHandler");
+        XRLog.setLevel("", Level.INFO);
     }
 
 }
