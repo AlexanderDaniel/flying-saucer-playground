e261fa514f0d4aa7054497c6974c40a6e7c6790a
Alexander Daniel <alexander.daniel@gmx.at>
2014-07-25 15:30:26 +0200

    Latin counter issue

---
+++ src/main/java/lachdrache/LatinCounterIssue.java
@@ -0,0 +1,9 @@
+package lachdrache;
+
+public class LatinCounterIssue {
+
+    public static void main(String[] args) throws Exception {
+        PdfGenerator.generatePdf("latinCounterIssue");
+    }
+
+}
